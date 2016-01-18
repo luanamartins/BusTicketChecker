2ecd1137bbb02a06434ec95e11736a59225737cd
Luana <lms7@cin.ufpe.br>
2016-01-18 00:48:38 -0300

    Fix timepicker instead of datepicker

--- app/src/main/java/checker/ticker/bus/busticketchecker/fragments/TimePickerFragment.java
+++ app/src/main/java/checker/ticker/bus/busticketchecker/fragments/TimePickerFragment.java
@@ -1,55 +1,34 @@
 package checker.ticker.bus.busticketchecker.fragments;
 
-
-import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.DatePicker;
+import android.widget.Button;
 import android.widget.TimePicker;
-import android.widget.Toast;
-
 import java.util.Calendar;
+import java.util.Locale;
 
 import checker.ticker.bus.busticketchecker.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class TimePickerFragment extends DialogFragment {
-
-
-    public TimePickerFragment() {
-        // Required empty public constructor
-    }
-
-
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int minute = calendar.get(Calendar.MINUTE);
-        TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
-
-            @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-
-
-            }
-        };
-        return new TimePickerDialog(getActivity(), listener, hour, minute, false);
+        return new TimePickerDialog(getActivity(), this, hour, minute, false);
     }
 
-    public void onDateSet(DatePicker view, int yy, int mm, int dd) {
-        populateSetDate(yy, mm+1, dd);
-    }
-    public void populateSetDate(int year, int month, int day) {
-
+    @Override
+    public void onTimeSet(TimePicker view, int hours, int minutes) {
+        Button time = (Button) getActivity().findViewById(R.id.newTimeButton);
+        String formattedHour = hours + ":";
+        if(minutes < 10){
+            formattedHour += "0";
+        }
+        formattedHour += minutes;
+        time.setText(formattedHour);
     }
 
 }
