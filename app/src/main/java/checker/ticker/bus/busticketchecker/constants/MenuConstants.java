13f9bc9896dc9f9792faeff73f2d2563266f6790
Luana <lms7@cin.ufpe.br>
2016-01-24 20:41:40 -0300

    Add class for constants

---
+++ app/src/main/java/checker/ticker/bus/busticketchecker/constants/MenuConstants.java
@@ -0,0 +1,8 @@
+package checker.ticker.bus.busticketchecker.constants;
+
+public class MenuConstants {
+    public int ADD_BUS_CARD = 0;
+    public int ADD_BUS_TAX = 1;
+    public int REMOVE_BUS_CARD = 2;
+    public int REMOVE_BUS_TAX = 3;
+}
