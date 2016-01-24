f51c3f53ed91c5d82673e4a2f21ba95418f099b5
Luana <lms7@cin.ufpe.br>
2016-01-24 20:53:32 -0300

    Update class for constants

--- app/src/main/java/checker/ticker/bus/busticketchecker/constants/MenuConstants.java
+++ app/src/main/java/checker/ticker/bus/busticketchecker/constants/MenuConstants.java
@@ -1,8 +1,8 @@
 package checker.ticker.bus.busticketchecker.constants;
 
 public class MenuConstants {
-    public int ADD_BUS_CARD = 0;
-    public int ADD_BUS_TAX = 1;
-    public int REMOVE_BUS_CARD = 2;
-    public int REMOVE_BUS_TAX = 3;
+    public static final int ADD_BUS_CARD = 0;
+    public static final int ADD_BUS_TAX = 1;
+    public static final int REMOVE_BUS_CARD = 2;
+    public static final int REMOVE_BUS_TAX = 3;
 }
