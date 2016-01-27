e268b3cf69361e5458c8f1e1166d8bbc3fdc64b3
Luana <lms7@cin.ufpe.br>
2016-01-26 22:01:11 -0300

    Get card name as user input in CardAdderFragment

--- app/src/main/java/fragments/CardAdderFragment.java
+++ app/src/main/java/fragments/CardAdderFragment.java
@@ -1,11 +1,13 @@
 package fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import checker.ticker.bus.basic.R;
 
@@ -13,7 +15,6 @@ import checker.ticker.bus.basic.R;
  * A simple {@link Fragment} subclass.
  */
 public class CardAdderFragment extends Fragment {
-
 
     public CardAdderFragment() {
         // Required empty public constructor
@@ -23,6 +24,9 @@ public class CardAdderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        EditText newCardNameEditText = (EditText) getActivity().findViewById(R.id.newCardNameEditText);
+        String newCardName = newCardNameEditText.getText().toString();
+        
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_card_adder, container, false);
     }
