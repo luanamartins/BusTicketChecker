a07f6f6038f0b44b9970f89d6ba6defa98db34d0
Luana <lms7@cin.ufpe.br>
2016-01-27 23:12:03 -0300

    Remove unused conditional

--- app/src/main/java/fragments/NavigationDrawerFragment.java
+++ app/src/main/java/fragments/NavigationDrawerFragment.java
@@ -116,6 +116,7 @@ public class NavigationDrawerFragment extends Fragment {
                 itemsForNavigationDrawer
         ));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
+
         return mDrawerListView;
     }
 
@@ -253,11 +254,6 @@ public class NavigationDrawerFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
             return true;
         }
 
