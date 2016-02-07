e88f41835f46c744828d099680ef49a52f0aff0c
Luana <lms7@cin.ufpe.br>
2016-02-07 12:58:53 -0300

    Get list of taxes from database

--- app/src/main/java/busticketchecker/fragments/BusTaxRemoverFragment.java
+++ app/src/main/java/busticketchecker/fragments/BusTaxRemoverFragment.java
@@ -12,38 +12,61 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import busticketchecker.database.SQLite.repository.TaxRepo;
+import busticketchecker.database.dao.BusCardDAO;
 import checker.ticker.bus.basic.R;
 
 public class BusTaxRemoverFragment extends Fragment
 {
+    private ArrayList<String> listOfTaxesNames;
+    private TaxRepo taxRepository;
+    private View rootView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_main_drawer, container, false);
+        this.rootView = inflater.inflate(R.layout.fragment_main_drawer, container, false);
+        this.setListOfTaxes(rootView);
+
+        return rootView;
+    }
+
+    private void setListOfTaxes(View rootView){
         final ListView list = (ListView) rootView.findViewById(R.id.listView2);
-        final String[] values = new String[]{"Tarifa 1", "Tarifa 2",};
 
-        final Context context = getActivity().getBaseContext();
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        this.taxRepository = new TaxRepo(rootView.getContext());
+        ArrayList<HashMap<String, String>> listOfTaxes = taxRepository.getTaxList();
+        this.listOfTaxesNames = new ArrayList<>();
+
+        int numberOfTaxesOnDatabase = listOfTaxes.size();
+
+        for (int i = 0; i < numberOfTaxesOnDatabase; i++)
+        {
+            listOfTaxesNames.add(listOfTaxes.get(i).get(BusCardDAO.KEY_NAME));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(rootView.getContext(), android.R.layout.simple_list_item_1, android.R.id.text1, listOfTaxesNames);
         list.setAdapter(adapter);
-
         list.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
             {
                 String remove = getString(R.string.remove);
-                String busTax = getString(R.string.bus_tax).toLowerCase();
+                String tax = getString(R.string.bus_tax).toLowerCase();
 
-                String title = remove + " " + busTax;
-                String message = remove + " " + values[position] + "?";
+                String taxName = listOfTaxesNames.get(position);
 
-                showDialog(title, message);
+                String title = remove + " " + tax;
+                String message = remove + " " + taxName + "?";
+
+                showDialog(title, message, taxName);
             }
         });
-        return rootView;
     }
 
     private void showDialog(String title, String message, String taxName)
@@ -53,10 +76,12 @@ public class BusTaxRemoverFragment extends Fragment
         builder.setMessage(message);
         String yes = getString(R.string.yes);
         String no = getString(R.string.no);
+        final String taxNameUnchangeable = taxName;
         builder.setPositiveButton(yes, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id)
             {
-                // TODO
+                taxRepository.delete(taxNameUnchangeable);
+                setListOfTaxes(rootView);
             }
         });
         builder.setNegativeButton(no, null);
