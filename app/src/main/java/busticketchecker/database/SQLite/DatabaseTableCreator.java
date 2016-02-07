3542299efb8d71863bb6e8c3ee5a1eea24266f10
Luana <lms7@cin.ufpe.br>
2016-02-07 12:11:43 -0300

    Fix column name of Tax table from value to cost

--- app/src/main/java/busticketchecker/database/SQLite/DatabaseTableCreator.java
+++ app/src/main/java/busticketchecker/database/SQLite/DatabaseTableCreator.java
@@ -8,7 +8,7 @@ public class DatabaseTableCreator
     public void createTables(SQLiteDatabase db)
     {
         String createTableCardQuery = "CREATE TABLE Card (card_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, type TEXT)";
-        String createTableTaxQuery = "CREATE TABLE Tax (tax_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, value FLOAT)";
+        String createTableTaxQuery = "CREATE TABLE Tax (tax_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, cost FLOAT)";
         String createTableTravelQuery = "CREATE TABLE Travel (travel_id INTEGER PRIMARY KEY AUTOINCREMENT, date DATE, card_name TEXT, card_type TEXT, tax_name TEXT, tax_value FLOAT, value FLOAT)";
 
         db.execSQL(createTableCardQuery);
