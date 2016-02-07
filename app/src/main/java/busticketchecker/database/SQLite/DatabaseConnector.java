e2ae4daf6e568b5c9f2d0d190b92440ce7a41be0
Luana <lms7@cin.ufpe.br>
2016-02-07 13:11:47 -0300

    Remove comments

--- app/src/main/java/busticketchecker/database/SQLite/DatabaseConnector.java
+++ app/src/main/java/busticketchecker/database/SQLite/DatabaseConnector.java
@@ -1,10 +1,5 @@
 package busticketchecker.database.SQLite;
 
-/**
- * http://www.101apps.co.za/index.php/articles/using-a-sqlite-database-in-android.html
- * http://instinctcoder.com/android-studio-sqlite-database-example/
- */
-
 public class DatabaseConnector
 {
 
